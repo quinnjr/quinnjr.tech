2d0e53bf0a1030f776afa8f35105786fd3f41426
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2022-04-18 20:16:59 -0400

    Fixing testing issue

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -10,7 +10,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomeComponent, ApolloTestingModule],
+      imports: [ApolloTestingModule],
+      declarations: [HomeComponent],
       providers: [FlashMessageService]
     }).compileComponents();
   });
