405f631c87c67be6307d905d3227ce93eb935ed2
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2023-10-03 13:46:53 -0400

    Adding compress flag to docker build

--- bin/build-container.sh
+++ bin/build-container.sh
@@ -6,6 +6,6 @@ cd ..
 
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 922147267922.dkr.ecr.us-east-2.amazonaws.com
 
-docker build -f docker/production.Dockerfile -t 922147267922.dkr.ecr.us-east-2.amazonaws.com/website:latest .
+docker build --compress -f docker/production.Dockerfile -t 922147267922.dkr.ecr.us-east-2.amazonaws.com/website:latest .
 
 docker push 922147267922.dkr.ecr.us-east-2.amazonaws.com/website:latest
